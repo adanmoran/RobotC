6543a92035fef52ded0dad854c8a6f9563824558
unknown <12amm19@BMH21414.appsci.queensu.ca>
2014-03-05 15:27:13 -0500

    Added the Project file (empty)

---
+++ Project.c
@@ -0,0 +1,22 @@
+
+/*This will be a colour-detecting robot: It will follow a line based on its colour pattern.
+ For example, if a center line is blue, there will be a green line on the right and
+ a red line on the left so that the robot can stay on track. It will display the colour
+ it is seeing on the screen, and when it reaches a black box at the end of the line it
+ will dance and "sing".*/
+
+//Declarations of defined words
+
+//Declarations of types
+
+//Declarations of functions
+void drive();
+void rotate();
+void senseColour();
+void celebrate();
+void displayImages();
+
+task main()
+{
+
+}
