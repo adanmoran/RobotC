ce42c160a1d2abca1aceb9d57aa7429834c5c326
unknown <12amm19@BMH21207.appsci.queensu.ca>
2014-03-19 16:08:33 -0400

    Ultrasonic works, calibration for light sensor works, sound sensor works. Need to add in celebration mechanism and screen.

--- Project.c
+++ Project.c
@@ -9,14 +9,14 @@
  The sound sensor will switch the direction of the robot.*/
 
 //Declarations of defined words
-#define SPEED 50
+#define SPEED 30
 	//Light level stuff
-#define FLOOR_LOWER 48
-#define FLOOR_HIGHER 53
-#define BLUE_LOWER 32
-#define BLUE_HIGHER 47
-#define RED_LOWER 0
-#define RED_HIGHER 0
+#define FLOOR_LOWER 47
+#define FLOOR_HIGHER 55
+#define BLUE_LOWER 30
+#define BLUE_HIGHER 40
+#define YELLOW_LOWER 59
+#define YELLOW_HIGHER 66
 #define BLACK_LOWER 0
 #define BLACK_HIGHER 0
 	//Sound level stuff
@@ -29,63 +29,62 @@
 //Declarations of functions
 void Drive(int speedLeft,int speedRight);
 int SenseColour();
-void celebrate();
-void displayImages();
+void Celebrate();
+void DisplayImages();
 void isLoud();
-int sonarDistance();
+void SonarDistance();
 //Declaration of global variables
-int lightLevel, soundLevel, ultraLevel, direction;
+int lightLevel, soundLevel, direction;
+float ultraLevel;
+bool finished;
 task main()
 {
 	//Initialize global variables
-	lightLevel = 0; soundLevel = 0; ultraLevel = 0; direction = 1;
+	lightLevel = 0; soundLevel = 0; ultraLevel = 1; direction = 1; finished = false;
 	//Declare sensors
-	SensorType[1] = sensorLightActive;
-	SensorType[2] = sensorSoundDB;
-	SensorType[3] = sensorSONAR;
+	SensorType[0] = sensorLightActive; //port 1
+	SensorType[1] = sensorSoundDB; //port 2
+	SensorType[2] = sensorSONAR; //port 3
 	//Enable PID on motors
 	nMotorPIDSpeedCtrl[1] = mtrSpeedReg;
 	nMotorPIDSpeedCtrl[2] = mtrSpeedReg;
 	//Declare variables
 	int colour, speed;
 	//Main loop
-	while(true)
+	while(true)// !finished)
 	{
+			//Check distance from objects in front.
+			SonarDistance();
 			//Check if direction is switching
 			isLoud();
-			speed = SPEED * direction;//*sonarDistance();
+			speed = SPEED * direction * ultraLevel;
 			colour = SenseColour();
 			nxtDisplayTextLine(2, "Sound level: %d", soundLevel);
 			nxtDisplayTextLine(3, "Light Level: %d", lightLevel);
 			nxtDisplayTextLine(4, "Speed: %d", speed);
 			nxtDisplayTextLine(5, "ColourType: %d", colour);
+			nxtDisplayTextLine(6, "Ultrasonic: %f", ultraLevel);
 			//Drive or rotate based on the colour of the tape
 			switch(colour)
 			{
-				case 0:
+				case 0: //On Floor
 					Drive(speed, speed);
 					break;
-				case 1:
+				case 1: //On Blue
 					Drive(speed/2, -speed/2);
 					break;
-				case 2:
-					Drive(0, 0);
+				case 2: //On Yellow
+					Drive(-speed/2, speed/2);
 					break;
-				case 3:
+				case 3: //On Black
 					Drive(0, 0);
+					finished = true;
 					break;
-				case 4:
+				case 4: //None of the above
 					Drive(0,0);
 					break;
 			}
-			//Drive(SPEED, SPEED);
-			//Debugging displays.
-			/*nxtDisplayTextLine(1, "Sound level: %d", soundLevel);
-			nxtDisplayTextLine(2, "Ultrasonic level:");
-			nxtDisplayTextLine(3, "  %d", ultraLevel);
-			nxtDisplayTextLine(4, "Light level: %d",lightLevel);*/
 		wait10Msec(20); //delay 200 msecs before next loop repetition
-		eraseDisplay();
 	}
 }
 
@@ -97,7 +96,7 @@ void Drive(int speedLeft, int speedRight)
 
 int SenseColour()
 {
-	lightLevel = SensorValue[1];
+	lightLevel = SensorValue[0];
 	int r = 4;
 
 	//Check type of reflectivity and return a normalized value from 0-3.
@@ -105,18 +104,18 @@ int SenseColour()
 			r = 0;
 	else if(lightLevel >= BLUE_LOWER && lightLevel <= BLUE_HIGHER)
 			r = 1;
-	/*else if(lightLevel >= RED_LOWER && lightLevel <= RED_HIGHER)
+	else if(lightLevel >= YELLOW_LOWER && lightLevel <= YELLOW_HIGHER)
 			r = 2;
-	else if(lightLevel >= BLACK_LOWER && lightLevel <= BLACK_HIGHER)
-			r = 3;
-	else
-			r = 4;*/
+	/*else if(lightLevel >= BLACK_LOWER && lightLevel <= BLACK_HIGHER)
+			r = 3;*/
 	return r;
 }
 
+/*
+Method switches direction of robot it sound sensor is triggered past preset loudness variable*/
 void isLoud()
 {
-	soundLevel = SensorValue[2];
+	soundLevel = SensorValue[1];
 
 	if(soundLevel > LOUD && direction == 1)
 	{
@@ -130,4 +129,18 @@ void isLoud()
 		direction = 1;
 		wait10Msec(20);
 	}
+}
+
+void SonarDistance()
+{
+	int distance = SensorValue[2];
+	nxtDisplayTextLine(7, "Distance: %d", distance);
+	float scale = 1;
+	if (distance >= SLOW_DISTANCE)
+		scale = 1.0;
+  else if (distance <= STOP_DISTANCE)
+  	scale = 0.0;
+ 	else
+ 		scale = ((float)distance/(SLOW_DISTANCE-STOP_DISTANCE)) - ((float)STOP_DISTANCE/(SLOW_DISTANCE-STOP_DISTANCE));
+ 	ultraLevel = scale;
 }
